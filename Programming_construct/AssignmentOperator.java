24988a2c2665793b9608c44e56883f45b4c047fc
ankitachaudharyy <ankitachaudharyy1@gmail.com>
2026-01-20 15:43:50 +0530

    [Ankita] added my file

---
+++ Programming_construct/AssignmentOperator.java
@@ -0,0 +1,18 @@
+class AssignmentOperator
+{
+	public static void main(String args[])
+	{ 
+		int a=10;
+		System.out.println(a++);
+		a+=5;
+		System.out.println(a);
+		a-=5;
+		System.out.println(a);
+		a*=5;
+		System.out.println(a);
+		a/=5;
+		System.out.println(a);
+		a%=5;
+		System.out.println(a);
+	}
+}	
